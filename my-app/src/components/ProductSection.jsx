b8942091e6fe1d059b7a1d5fd2683d5965360a4d
NicoolDqek <136205493+NicoolDqek@users.noreply.github.com>
2025-06-06 00:13:52 -0500

    categories

--- my-app/src/components/ProductSection.jsx
+++ my-app/src/components/ProductSection.jsx
@@ -2,14 +2,19 @@ import React, { useContext, useState } from 'react'
 import {AppContextV} from '../context/AppContext'
 import ProductCard from './ProductCard'
 function ProductSection() {
-    const {productList}=useContext(AppContextV)
-     
+    const {productList,Categories}=useContext(AppContextV)
+  
   
 
     const best=productList.filter(b=>b.rating > 4.7)
     return (
     <div>
       <h1>Productos Destacados</h1>
+      <div>
+        {Categories.map((c,index)=>(
+            <button key={index}>{c.name}</button>
+        ))}
+      </div>
       <div>
       {best.map(p=>(
     <div key={p.id}>
