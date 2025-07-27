4e48736872ad51b00ac662eb1aea69c2243f533a
NicoolDqek <136205493+NicoolDqek@users.noreply.github.com>
2025-07-27 00:14:28 -0500

    Fix: corrected import case for CardDetails

--- my-app/src/pages/ProductDetails.jsx
+++ my-app/src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import CardDetails from "../components/cardDetails"
+import CardDetails from "../components/CardDetails"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 
